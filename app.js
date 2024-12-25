42d8619f208b8ba45662f55c2975fbff2b2db99f
DaniDipp <dippigames@gmail.com>
2024-12-25 12:34:20 +0000

    dUpdate

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-const express = require("express");
+import express from "express";
+import PocketBase from "pocketbase";
+import { EventSource } from 'eventsource'
+global.EventSource = EventSource;
+
+const pocketbase = new PocketBase("https://db.rawb.tv");
+let value = -1;
+pocketbase.collection("lom2_magicspells").getOne("2joauwmh4zsos5p").then((doc) => {
+	value = Number(doc.value);
+	console.log("value is " + value);
+});
+pocketbase.collection("lom2_magicspells").subscribe("2joauwmh4zsos5p", (e) => {
+	console.log("Realtime update: " + e.record);
+	value = Number(e.record.value);
+	console.log("value is " + value);
+});
+
 const app = express();
 
+app.use((req, res, next) => {
+	res.setHeader(
+		'Content-Security-Policy',
+		"default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
+	);
+	next();
+});
+
+
+
+
+
 app.use('/assets', express.static('assets'));
+app.use("/pb", express.static("node_modules/pocketbase/dist"));
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname);
+app.set('views', ".");
 
 app.get('/', function (req, res) {
-	const doom = new Date(1735189200000);
+	const doom = new Date(1735189200000 + value);
 	var now = new Date();
 	var diffMs = parseInt((doom - now) / 1000);
 	var string = diffMs.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
